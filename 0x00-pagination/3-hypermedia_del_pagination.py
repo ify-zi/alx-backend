66b25c823eebee6b07f2d5d973ec2fc1b80a21ab
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-21 20:21:45 +0000

    Pagination: using hypermedia pagination to keep track of deleted items

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> dict:
+        """dataset return with other info"""
+        dataset = self.indexed_dataset()
+        assert index is not None and index < len(dataset)
+
+        idx = index
+        keys = list(dataset.keys())
+        while idx not in keys:
+            idx += 1
+
+        data = keys[keys.index(idx): keys.index(idx) + page_size]
+
+        return {
+                'index': index,
+                'next_index': idx + len(data),
+                'page_size': len(data),
+                'data': [dataset[key] for key in data],
+                }
