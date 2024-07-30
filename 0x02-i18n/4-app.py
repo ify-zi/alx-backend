12faac0eb84aada3e4bf72e7797ecebd067d6249
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-30 20:19:33 +0000

    using Query to determine lang

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    i18n Flask Babel module
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """default parameters in babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get the best match for language"""
+    lang = request.args.get("locale")
+    if lang in app.config["LANGUAGES"]:
+        return lang
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/', strict_slashes=False)
+def greet():
+    """return index from template"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
