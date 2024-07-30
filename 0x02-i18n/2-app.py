47ad3c4fc934ffc2eff1f3e561ba47774bc13c22
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-30 19:13:47 +0000

    Adding the get_locale() to retrive the best match for lang

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+    i18n Flask Babel module
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from pytz import UTC
+
+
+class Config(object):
+    """default parameters in babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+babel = Babel()
+
+app = Flask(__name__)
+babel.init_app(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get the best match for language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/', strict_slashes=False)
+def greet():
+    """return index from template"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000)
