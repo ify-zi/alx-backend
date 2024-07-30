415f1938f3a0ed58b0867045bd8f47fca2587dfe
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-30 17:46:02 +0000

    Updated the 1-app with class of deafutl parameters for conig

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-class Config:
+class Config(object):
     """default parameters in babel"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_TIMEZONE = 'UTC'
