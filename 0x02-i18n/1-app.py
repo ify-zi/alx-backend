7df64846fdc1e73ba4e29c5b68c1a87cf71a822f
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-30 17:36:47 +0000

    Babel i18n task

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    i18n Flask Babel module
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+babel = Babel(app)
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+
+
+@app.route('/', strict_slashes=False)
+def greet():
+    """return index from template"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000)
