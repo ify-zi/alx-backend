3f8150874ffba328d5814556151b9ed3f7c003b7
Ifeanyi Zion Chidiebere <ifeanyizion3@gmail.com>
2024-07-30 19:28:55 +0000

    Adding the get_locale() to retrive the best match for lang

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,7 +20,7 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale()-> str:
     """get the best match for language"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
